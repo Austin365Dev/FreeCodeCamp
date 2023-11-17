fbe9c8409622ec69caab91cc61deee80e81b8602
Htoo Thet <htoothetnaung@Htoos-MacBook-Pro.local>
2023-11-17 20:23:01 +0630

    Build if.js

---
+++ if.js
@@ -0,0 +1,28 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if(strokes == 1){
+console.log( names[0]);
+  }
+  else if(strokes <= par -2){
+console.log( names[1]);
+  }
+  else if(strokes == par-1){
+console.log( names[2]);
+  }
+  else if(strokes == par+1){
+console.log( names[3]);
+  }
+  else if(strokes == par+2){
+console.log( names[4]);
+  }
+  else if(strokes >= par+3){
+console.log( names[5]);
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
