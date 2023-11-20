8eadf5efcbef4e409ae92d6f15544c1ac1cf94b2
HtooThetNaung <92380477+Austin365Dev@users.noreply.github.com>
2023-11-20 20:04:05 +0630

    How to generate selected range of random numbers

---
+++ BasicJs/generateRandom.js
@@ -0,0 +1,18 @@
+// how to generate random number from your selected range
+
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin +1)) + myMin;
+}
+  
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+console.log(randomRange(1, 45));
+  
+  
