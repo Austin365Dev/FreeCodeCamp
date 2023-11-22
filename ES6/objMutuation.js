548df749e181263a0931fb955e36cf1b8fe75f67
HtooThetNaung <92380477+Austin365Dev@users.noreply.github.com>
2023-11-22 19:08:08 +0630

    preventing object mutuation

---
+++ ES6/objMutuation.js
@@ -0,0 +1,18 @@
+// in js objects and functions and arrays declared with const are still mutuable so you need to do something to prvenet unauthorized alteration by Object.freeze
+
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
