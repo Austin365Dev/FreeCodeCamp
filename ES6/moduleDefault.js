c595845567faa556d6aef85f0ed909f339fa46e7
HtooThetNaung <92380477+Austin365Dev@users.noreply.github.com>
2023-11-23 16:08:29 +0630

    JS Module

---
+++ ES6/moduleDefault.js
@@ -0,0 +1,8 @@
+// normal import , export and default import export
+
+import { add, subtract } from './math_functions.js';
+
+import subtract from "./math_functions.js";  
+// Only change code above this line
+
+subtract(7,4);
